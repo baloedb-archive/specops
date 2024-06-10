7299559a8de21318374d5248c85f26d20ac34978
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-06-10 21:33:42 +0200

    Update backend

--- backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
+++ backend/src/test/java/nl/itvitae/specops/SpecopsApplicationTests.java
@@ -1,13 +1,11 @@
 package nl.itvitae.specops;
 
+// Test not working, should look into it.
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpecopsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
